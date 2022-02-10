ab4d6c7699909dc3154e35381a7758427a8e6901
ShawnSwitzer <82423401+ShawnSwitzer@users.noreply.github.com>
2022-02-09 18:03:10 -0600

    Update lcd_displayFix.ino
    Simplified code. Prints line by line instead of the monstrosity I had before.

--- lcd_displayFix.ino
+++ lcd_displayFix.ino
@@ -112,10 +112,10 @@ byte bottomLeftSCurve[] = {
 void setup()
 
 {        
-  //Initialized screen and backlight.
+   //Initialized screen and backlight.
   lcd.init();
   lcd.backlight();
-  
+
   //Characters used to create the logo
   lcd.begin(20, 4);
   lcd.createChar(0, fullBox);
@@ -126,86 +126,101 @@ void setup()
   lcd.createChar(5, sideLeftAInnerChar);
   lcd.createChar(6, sideLeftInnerCharConnector);
   lcd.createChar(7, topRightSCurve);
-  //Creates Letter "E"
-  int y1 = 1;
-  for(int x = 13; x<18; x++)
-  {
-    lcd.setCursor(x,0);
-    lcd.write(0);  
-    lcd.setCursor(x-3,0);
-    lcd.write(0);
-    lcd.setCursor(13,y1);
-    lcd.write(0);
-    lcd.setCursor(x,3);
-    lcd.write(0);
-    lcd.setCursor(12,y1);
-    lcd.write(0);
-    y1++;
-  }
 
-
-  //Creates Letter A
-   for(int x = 14; x<18; x++)
+  /**
+   * First line of LCD Screen
+   */
+  lcd.setCursor(1,0);
+  lcd.write(3);
+  
+  for(int i = 2; i <= 5; i++)
   {
-    lcd.setCursor(x,1);
-    lcd.write(1);
-    lcd.setCursor(x,2);
-    lcd.write(2);  
+    lcd.setCursor(i,0);
+    lcd.write(0);
   }
+  
+  lcd.setCursor(6,0);
+  lcd.write(7);
 
   lcd.setCursor(9,0);
   lcd.write(3);
-  lcd.setCursor(8,1);
-  lcd.write(4);
-  lcd.setCursor(7,2);
-  lcd.write(4);
-  lcd.setCursor(6,3);
-  lcd.write(0);
-  lcd.setCursor(9,1);
-  lcd.write(5);
-  lcd.setCursor(7,3);
-  lcd.write(5);
-  lcd.setCursor(8,2);
-  lcd.write(0);
 
-  for(int x = 9; x<12; x++)
+  for(int i = 10; i <= 17; i++)
   {
-    lcd.setCursor(x,2);
+    lcd.setCursor(i,0);
     lcd.write(0);  
   }
 
-  //Creates Letter S
-  for(int x  = 2; x<6; x++)
-  {
-      lcd.setCursor(x,0);
-      lcd.write(0);
-  }
-  lcd.setCursor(6,0);
-  lcd.write(7);
-  lcd.setCursor(6,1);
-  lcd.write(2);
-  lcd.setCursor(1,0);
-  lcd.write(3);
+  /**
+   * Second line of LCD Screen
+   */
   lcd.setCursor(1,1);
   lcd.write(0);
-  lcd.setCursor(1,2);
+  lcd.setCursor(6,1);
   lcd.write(2);
+  lcd.setCursor(8,1);
+  lcd.write(4);
+  lcd.setCursor(9,1);
+  lcd.write(5);
+  lcd.setCursor(12,1);
+  lcd.write(0);
+  lcd.setCursor(13,1);
+  lcd.write(0);
 
-  for(int x = 2; x<6; x++)
+  for(int i = 14; i <= 17; i++)
   {
-    lcd.setCursor(x,2);
-    lcd.write(2);
+    lcd.setCursor(i,1);
+    lcd.write(1);
   }
-  lcd.setCursor(6,2);
-  lcd.write(7);
-  lcd.setCursor(1,3);
-  lcd.write(6);
 
-  for(int x = 2; x<6;x++)
-  {
-    lcd.setCursor(x,3);
-    lcd.write(1);  
-  }
+  /**
+   * Third line of LCD Screen
+   */
+   for(int i = 1; i <= 5; i++)
+   {
+      lcd.setCursor(i,2);
+      lcd.write(2);
+   }
+   
+   lcd.setCursor(6,2);
+   lcd.write(7);
+   lcd.setCursor(7,2);
+   lcd.write(4);
+
+   for(int i = 8; i <= 13; i++)
+   {
+      lcd.setCursor(i,2);
+      lcd.write(0); 
+   }
+
+   for(int i = 14; i <= 17; i++)
+   {
+      lcd.setCursor(i,2);
+      lcd.write(2);
+   }
+
+   /**
+    * Fourth line of LCD Screen
+    */
+    lcd.setCursor(1,3);
+    lcd.write(6);
+
+    for(int i = 2; i <= 5; i++)
+    {
+       lcd.setCursor(i,3);
+       lcd.write(1);
+    }
+
+    lcd.setCursor(6,3);
+    lcd.write(0);
+    lcd.setCursor(7,3);
+    lcd.write(5);
+
+    for(int i = 12; i <= 17; i++)
+    {
+        lcd.setCursor(i,3);
+        lcd.write(0);  
+    }
 }
 
 //Optional code to make logo blink on the lcd screen.
